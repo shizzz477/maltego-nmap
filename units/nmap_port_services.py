f194682648eb32d425ff8d5d698f4590e6da0cd1
shizzz477 <marcgurreri@gmail.com>
2017-07-01 21:32:50 +0900

    upd

--- units/nmap_port_services.py
+++ units/nmap_port_services.py
@@ -3,14 +3,14 @@ from MaltegoTransform import *
 m = MaltegoTransform()
 m.parseArguments(sys.argv)
 
-ports = m.getVar('service.ports') # string
+ports = m.getVar('service.ports')  # string
 ports = ports.split('|')  # array
 
 for port in ports:
-  port, name = port.split('::')
+	port, name = port.split('::')
 
-  svc = m.addEntity('maltego.Service', port)
-  svc.addAdditionalFields("port.number", "Port", "strict", port)
-  svc.addAdditionalFields("banner.text", "Service Banner", "strict", name)
+	svc = m.addEntity('maltego.Service', port)
+	svc.addAdditionalFields("port.number", "Port", "strict", port)
+	svc.addAdditionalFields("banner.text", "Service Banner", "strict", name)
 
 m.returnOutput()
--- units/nmap_xml_parser.py
+++ units/nmap_xml_parser.py
@@ -1,7 +1,7 @@
 import fnmatch, os
 
 from MaltegoTransform import *
-from lxml             import etree
+from lxml import etree
 
 m = MaltegoTransform()
 m.parseArguments(sys.argv)
@@ -11,43 +11,43 @@ filename = m.getValue()
 results = []
 
 if os.path.isdir(filename):
-  for root, dirnames, filenames in os.walk(filename):
-    for result in fnmatch.filter(filenames, 'results.xml'):
-      results.append(os.path.join(root, result))
+	for root, dirnames, filenames in os.walk(filename):
+		for result in fnmatch.filter(filenames, 'results.xml'):
+			results.append(os.path.join(root, result))
 else:
-  results.append(filename)
+	results.append(filename)
 
 for result in results:
-  document = etree.parse(result)
+	document = etree.parse(result)
 
-  for host in document.xpath("//host[ports/port[state[@state='open']]]"):
-    iface = None
+	for host in document.xpath("//host[ports/port[state[@state='open']]]"):
+		iface = None
 
-    for addr in host.xpath("address[@addrtype='ipv4']"):
-      iface = m.addEntity("maltego.IPv4Address", addr.attrib['addr'])
+		for addr in host.xpath("address[@addrtype='ipv4']"):
+			iface = m.addEntity("maltego.IPv4Address", addr.attrib['addr'])
 
-    if iface != None:
-      ports = []
+		if iface != None:
+			ports = []
 
-      for port in host.xpath("ports/port[state[@state='open']]"):
-        pnum = port.attrib['portid']
-        name = []
+			for port in host.xpath("ports/port[state[@state='open']]"):
+				pnum = port.attrib['portid']
+				name = []
 
-        try:
-          svc = port.xpath("service")[0]
+				try:
+					svc = port.xpath("service")[0]
 
-          try:
-            name.append(svc.attrib['product'])
-            name.append(svc.attrib['version'])
-            name.append(svc.attrib['extrainfo'])
-          except KeyError:
-            if len(' '.join(name)) == 0:
-              name.append(svc.attrib['name'])
-        except:
-          pass
+					try:
+						name.append(svc.attrib['product'])
+						name.append(svc.attrib['version'])
+						name.append(svc.attrib['extrainfo'])
+					except KeyError:
+						if len(' '.join(name)) == 0:
+							name.append(svc.attrib['name'])
+				except:
+					pass
 
-        ports.append("%s::%s" % (pnum, ' '.join(name)))
+				ports.append("%s::%s" % (pnum, ' '.join(name)))
 
-      iface.addAdditionalFields("service.ports", "Listening Ports", "strict", '|'.join(ports))
+			iface.addAdditionalFields("service.ports", "Listening Ports", "strict", '|'.join(ports))
 
 m.returnOutput()
